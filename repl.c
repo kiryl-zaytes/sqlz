03b92b6fc0e6c4206f05ffa60f2ef8d0f4865c5c
Kiryl Zayets <zayetsk@amazon.com>
2021-01-19 16:57:45 -0800

    Added row representation, serialize and des to memory location

--- repl.c
+++ repl.c
@@ -2,7 +2,26 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 
+#define COL_USERNAME_SIZE 32
+#define COL_EMAIL_SIZE 255
+#define size_of_attr(TYPE, ATTR) sizeof(((TYPE*)0)->ATTR) // start counting as it was 0 address + real size of attr;
+#define ID_OFFSET 0
+#define get_user_offset() ID_OFFSET+ID_SIZE
+#define get_email_offset(USER_OFFSET) USER_OFFSET+USERNAME_SIZE
+#define get_row_size() ID_SIZE + USERNAME_SIZE + EMAIL_SIZE
+
+typedef struct {
+    uint32_t id;
+    char username[COL_USERNAME_SIZE];
+    char email[COL_EMAIL_SIZE];
+} Row;
+
+// Rows representation
+const uint32_t ID_SIZE = size_of_attr(Row, id);
+const uint32_t USERNAME_SIZE = size_of_attr(Row, username);
+const uint32_t EMAIL_SIZE = size_of_attr(Row, email);
 
 typedef struct {
     char* buffer;
@@ -17,6 +36,7 @@ typedef enum {
 
 typedef enum {
     IND_SUCCESS,
+    IND_SYNTAX_ERROR,
     IND_UNRECOGNIZED
 } IndexCommand;
 
@@ -27,6 +47,7 @@ typedef enum {
 
 typedef struct {
     SupportedOperations operation;
+    Row row_to_insert;
 } Statement;
 
 void free_memorybuffer(InputBuffer* ib){
@@ -47,6 +68,8 @@ OperationalCommand doOperationalCommand(InputBuffer* ib){
 IndexCommand doIndexCommand(InputBuffer* ib, Statement* statement){
     if (strncmp(ib->buffer, "insert", 6) == 0){
         statement->operation = INSERT;
+        int arg_size = sscanf(ib->buffer, "insert %d %s %s", &(statement->row_to_insert.id), statement->row_to_insert.username, statement->row_to_insert.email);
+        if (arg_size < 3) return IND_SYNTAX_ERROR;
         return IND_SUCCESS;
     }
     if(strncmp(ib->buffer, "select", 6) == 0){
@@ -83,6 +106,18 @@ void read_input(InputBuffer* ib){
 
 }
 
+void serialize_memory_index(Row* source, void* destination){
+    memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
+    memcpy(destination + get_user_offset(), &(source->username), USERNAME_SIZE);
+    memcpy(destination + get_email_offset(), &(source->email), EMAIL_SIZE);
+}
+
+void desirialize_memory_index(void* source, Row* destination){
+    memcpy(&(destination->id),source + ID_OFFSET, ID_SIZE);
+    memcpy(&(destination->username),source + get_user_offset(), USERNAME_SIZE);
+    memcpy(&(destination->email),source + get_email_offset(),EMAIL_SIZE);
+}
+
 void main(int argc, char* argv[]){
     InputBuffer* ib = new_buffer();
     while (true){
@@ -91,11 +126,11 @@ void main(int argc, char* argv[]){
         if (ib->buffer[0] == '.'){
             switch (doOperationalCommand(ib))
             {
-            case OP_SUCCES:
-                continue;
-            case OP_UNRECOGNIZED: 
-                printf("Unsupported command %s ", ib->buffer);
-                continue;
+                case OP_SUCCES:
+                    continue;
+                case OP_UNRECOGNIZED: 
+                    printf("Unsupported command %s ", ib->buffer);
+                    continue;
             }
         }
 
